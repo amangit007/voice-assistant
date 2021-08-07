b1a6a1eed4c7bf532e1bced76097fafa2864c7e2
amangit007 <65654655+amangit007@users.noreply.github.com>
2021-08-07 15:26:02 +0530

    Update assistant_name.py
    Naming Assistant

--- assistant_name.py
+++ assistant_name.py
@@ -1,1 +1,6 @@
-print()
+
+def set_name()
+    # Remove Alexa to name assistant anything else
+    # after this assistant will only reply when called with specified name
+    name='Alexa'
+    return name
